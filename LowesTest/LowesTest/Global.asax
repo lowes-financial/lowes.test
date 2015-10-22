9aaff25c6329613eaeae36f5594d935524b4cdb2
lowes.financial <alan.watson@lowes.co.uk>
2015-10-22 15:17:06 +0100

    Initial commit

---
+++ LowesTest/LowesTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LowesTest.MvcApplication" Language="C#" %>
---
+++ LowesTest/LowesTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LowesTest.Startup))]
+namespace LowesTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
